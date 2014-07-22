df9bd9dd6e6a69e336d3537d29d52de6b7a0cfa5
ypershin <ypershin114@gmail.com>
2014-07-21 18:14:49 -0600

    Create R_script.R

---
+++ R_script.R
@@ -0,0 +1,4 @@
+library("RODBC")
+library("plyr", lib.loc="C:/Program Files/R/R-3.0.3/library")
+library("reshape", lib.loc="C:/Program Files/R/R-3.0.3/library")
+# args <- commandArgs(trailingOnly = TRUE)
